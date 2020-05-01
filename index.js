0e46d33d0279f272f2c108fb40b08857e12362ef
Cristian Villada <cristianmilo07@gmail.com>
2020-05-01 18:00:36 -0500

    Realizando peticiones

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ const port = process.env.PORT || 3001
 app.use(bodyParse.urlencoded({ extended: false }))
 app.use(bodyParse.json())
 
+app.get('/hola/:name', (req, res) => {
+    res.send({message : `Hola ${req.params.name}!`})
+})
+
+
+/* ... peticiones  */
+
+app.get('/api/product', (req, res) => {
+    res.send(200, {products: []})
+})
+
+app.get('/api/product/:productId', (req, res) => {
+    
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: 'El producto se ha recibido'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+
+})
+
 app.listen(port, () => {
     console.log(`API REST corriendo en http://localhost:${port}`)
 })
